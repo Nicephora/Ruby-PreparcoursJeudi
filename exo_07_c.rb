0665aa2047718f703e45842baa6ccb3ee1f4b911
nicephore <nicephorevictor@gmail.com>
2019-03-16 00:38:55 +0100

    Exercies 07 a b et c

---
+++ exo_07_c.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+puts user_name
+
+#le a marche avec la phrase
+#le b rajoute ">" avant la saisie, c'est le plus abouti des 3
+#le c marche mais il n'y a pas de phrase avant la saisie du coup on ne sait pas quest ce que l'on doit faire quand on lance le programme
