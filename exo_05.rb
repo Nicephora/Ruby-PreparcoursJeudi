4fe9a0d7b989a6478d5310d8a0b49c5990817de4
nicephore <nicephorevictor@gmail.com>
2019-03-16 00:35:47 +0100

    Une des variable na pas été définit

---
+++ exo_05.rb
@@ -0,0 +1,24 @@
+puts "On va compter le nombre d'heures de travail à THP" # affiche la phrase entre les "" dans la console
+puts "Travail : #{10 * 5 * 11}" # Travail : 550
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #output : En minute ça fait: 33000
+
+puts "Et en secondes ?" #affiche la phrase entre les "" dans la console
+
+puts 10 * 5 * 11 * 60 * 60 #1980000
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #affiche la phrase entre les "" dans la console
+
+puts 3 + 2 < 5 - 7 #false
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"  #Ça fait combien 3 + 2 ? 5
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #Ça fait combien 5 - 7 ? -2
+
+puts "Ok, c'est faux alors !" #Ok, c'est faux alors !
+
+puts "C'est drôle ça, faisons-en plus :" #C'est drôle ça, faisons-en plus :
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #Est-ce que 5 est plus grand que -2 ? true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #Est-ce que 5 est supérieur ou égal à -2 ? true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #Est-ce que 5 est inférieur ou égal à -2 ? false
+
+# #{} c'est une string interpolation
